37fe46bff17217cc063493cf4cff7cffa16baef9
Caio Oliveira <oliveirac@vmware.com>
2021-03-22 17:33:30 -0300

    changed variables.tf

--- variables.tf
+++ variables.tf
@@ -28,18 +28,18 @@ variable "resource_group_location" {
     description = "Azure Resource Group Location"
 }
 
-variable "azurerm_vmware_private_cloud_name" {
-    description = "Name of the AVS SDDC"
-}
+#variable "azurerm_vmware_private_cloud_name" {
+#    description = "Name of the AVS SDDC"
+#}
 
-variable "azurerm_vmware_private_cloud_sku" {
-    description = "The host type used for AVS"
-}
+#variable "azurerm_vmware_private_cloud_sku" {
+#    description = "The host type used for AVS"
+#}
 
-variable "azurerm_vmware_private_cloud_management_cidr" {
-    description = "This is the management network CIDR range"
-}
+#variable "azurerm_vmware_private_cloud_management_cidr" {
+#    description = "This is the management network CIDR range"
+#}
 
-variable "azurerm_vmware_private_cloud_host_count" {
-    description = "Number of hosts in the SDDC"
-}
+#variable "azurerm_vmware_private_cloud_host_count" {
+#    description = "Number of hosts in the SDDC"
+#}
