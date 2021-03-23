291d68a2c611725be5305a5e781b443e658e693c
Caio Oliveira <oliveirac@vmware.com>
2021-03-23 13:25:16 -0300

    main 2

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 # Getting Authorization Key on AVS for Connection setup on Virtual Network Gateway
 
-resource "azurerm_express_route_circuit_authorization" "example" {
+resource "azurerm_express_route_circuit_authorization" "examplenew" {
   name                       = "exampleERCAuth"
   express_route_circuit_name = "tnt26-cust-p03-eastus-er"
   resource_group_name        = "tnt26-cust-p03-eastus"
