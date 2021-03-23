417e7046aa1da4894dd4881c2b28cb0753d592a2
Caio Oliveira <oliveirac@vmware.com>
2021-03-23 08:48:29 -0300

    Separate subnets from VNET

--- main.tf
+++ main.tf
@@ -15,31 +15,34 @@ resource "azurerm_resource_group" "example" {
 }
 
 resource "azurerm_virtual_network" "example" {
-  name                = "virtualNetwork1-terraform"
+  name                = "VNET-Terraform"
   location            = azurerm_resource_group.example.location
   resource_group_name = azurerm_resource_group.example.name
   address_space       = ["192.168.100.0/24"]
 
-  subnet {
-    name           = "subnet1"
-    address_prefix = "192.168.100.0/26"
-  }
-
   tags = {
-    environment = "Production"
+    environment = "Tests Terraform"
   }
 }
 
 resource "azurerm_subnet" "examplesubnet" {
-  name                 = "subnet2"
+  name                 = "Subnet-Terraform-01"
+  resource_group_name  = azurerm_resource_group.example.name
+  virtual_network_name = azurerm_virtual_network.example.name
+  address_prefixes     = ["192.168.100.0/26"]
+
+}
+
+resource "azurerm_subnet" "examplesubnet2" {
+  name                 = "Subnet-Terraform-02"
   resource_group_name  = azurerm_resource_group.example.name
   virtual_network_name = azurerm_virtual_network.example.name
   address_prefixes     = ["192.168.100.128/26"]
 
 }
 
-resource "azurerm_subnet" "examplesubnet2" {
-  name                 = "subnet3"
+resource "azurerm_subnet" "examplesubnet3" {
+  name                 = "Subnet-Terraform-03"
   resource_group_name  = azurerm_resource_group.example.name
   virtual_network_name = azurerm_virtual_network.example.name
   address_prefixes     = ["192.168.100.192/26"]
