c47e6814f4c907b72c5d774ac427fa67b6f81a8c
Caio Oliveira <oliveirac@vmware.com>
2021-03-23 16:01:26 -0300

    sanitize code

--- main.tf
+++ main.tf
@@ -13,31 +13,31 @@ provider "azurerm" {
 
 # Creating Resource Group
 
-resource "azurerm_resource_group" "example" {
+resource "azurerm_resource_group" "rg" {
   name     = var.resource_group_name
   location = var.resource_group_location
 }
 
 # Creating VNET
 
-resource "azurerm_virtual_network" "example" {
+resource "azurerm_virtual_network" "vnet" {
   name                = "VNET-Terraform"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
   address_space       = ["192.168.100.0/24"]
 
   subnet {
-    name           = "subnet1"
+    name           = "vnet-subnet1"
     address_prefix = "192.168.100.64/26"
   }
 
   subnet {
-    name           = "subnet2"
+    name           = "vnet-subnet2"
     address_prefix = "192.168.100.128/26"
   }
 
   subnet {
-    name           = "subnet3"
+    name           = "vnet-subnet3"
     address_prefix = "192.168.100.192/26"
   }
 
@@ -57,33 +57,33 @@ resource "azurerm_virtual_network" "example" {
 
 data "azurerm_subnet" "gateway_subnet" {
   name                 = "GatewaySubnet"
-  virtual_network_name = azurerm_virtual_network.example.name
-  resource_group_name  = azurerm_resource_group.example.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  resource_group_name  = azurerm_resource_group.rg.name
 }
 
 # Requesting Public IP
 
-resource "azurerm_public_ip" "example" {
+resource "azurerm_public_ip" "publicIP" {
   name                = "test"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
 
   allocation_method = "Dynamic"
 }
 
 # Creating Virtual Network Gateway
 
-resource "azurerm_virtual_network_gateway" "example" {
+resource "azurerm_virtual_network_gateway" "vng" {
   name                = "test"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
 
   type     = "ExpressRoute"
   sku      = "Standard"
 
   ip_configuration {
     name                          = "vnetGatewayConfig"
-    public_ip_address_id          = azurerm_public_ip.example.id
+    public_ip_address_id          = azurerm_public_ip.publicIP.id
     private_ip_address_allocation = "Dynamic"
     subnet_id                     = data.azurerm_subnet.gateway_subnet.id
   }
@@ -106,12 +106,12 @@ data "azurerm_vmware_private_cloud" "avs_sddc" {
 }
 
 resource "azurerm_virtual_network_gateway_connection" "connection" {
-  name                = "vNET_to_AVS"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
+  name                = "VNET_to_AVS"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
 
   type                            = "ExpressRoute"
-  virtual_network_gateway_id      = azurerm_virtual_network_gateway.example.id
+  virtual_network_gateway_id      = azurerm_virtual_network_gateway.vng.id
 
   authorization_key = var.authorization_key
   express_route_circuit_id = data.azurerm_vmware_private_cloud.avs_sddc.circuit[0].express_route_id
